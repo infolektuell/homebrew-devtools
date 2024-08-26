b66b3f3c9af859e62c37e38c5d7da6ba24344930
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-08-26 14:57:04 +0200

    Add formula: jextract

---
+++ Formula/jextract.rb
@@ -0,0 +1,25 @@
+class Jextract < Formula
+  desc "Jextract mechanically generates Java bindings from native library headers."
+  homepage "https://jdk.java.net/jextract/"
+  version "22"
+  license "gplv2+ce"
+  on_macos do
+    on_arm do
+      url "https://download.java.net/java/early_access/jextract/22/5/openjdk-22-jextract+5-33_macos-aarch64_bin.tar.gz"
+      sha256 "2a4411c32aedb064c3e432eb8a2791e6e60fea452330c71386f6573dc4c9c850"
+    end
+    on_intel do
+      url "https://download.java.net/java/early_access/jextract/22/5/openjdk-22-jextract+5-33_macos-x64_bin.tar.gz"
+      sha256 "0f65d480a1713d73c179e91f3ab6b9553c22694cd1a9f7936ffa8ca351d12390"
+    end
+  end
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script (libexec/"bin/jextract")
+  end
+
+  test do
+    system bin/"jextract", "--version"
+  end
+end
